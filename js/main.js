ec3dc14318b7e39787ca8e4669ee7d80fddd7dc2
sidd sirius <sidd.benoit@gmail.com>
2021-03-07 08:26:48 -0500

    menu mobile

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Ouvrir et fermer le menu de navigation
+const btnMenu = document.querySelector(".menu-nav");
+const menuNav = document.querySelector("#menu");
+
+btnMenu.addEventListener("click", () => {
+    btnMenu.classList.toggle("ouvrir");
+    if (btnMenu.classList.contains("ouvrir")) {
+        btnMenu.innerHTML = "F e r m e r";
+    } else {
+        btnMenu.innerHTML = "M e n u";
+    }
+    menuNav.classList.toggle("ouvrir");
+})
